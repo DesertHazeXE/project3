7d7451c971ae4de3d681460910f1ee6cda039d7d
DesertHazeXE <davidtg@code.edu.az>
2021-12-28 12:29:42 +0400

    first commit

---
+++ Department.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResourceManagement.Models
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public int WorkerLimit { get; set; }
+        public double SalaryLimit { get; set; }
+        public Employee[] Employees { get; set; }
+        public int CurrentIndex { get; set; }
+
+
+
+        public Department(string name, int workerlimit, double salarylimit)
+        {
+            
+
+            Employees = new Employee[workerlimit];
+            Name = name;
+            WorkerLimit = workerlimit;
+            SalaryLimit = salarylimit;
+
+        }
+        public double CalcSalaryAverage()
+        {
+            if (CurrentIndex == 0)
+                return 0;
+            double totalSalary = 0;
+            int count = 0;
+            foreach (Employee item in Employees)
+            {
+                if (item != null)
+                {
+                    totalSalary += item.Salary;
+                    count++;
+
+                }
+                else
+                {
+                    break;
+                }
+
+
+
+
+
+            }
+            return totalSalary / count;
+
+
+
+        }
+
+        public void AddEmployee(Employee emp1)
+        {
+            if (CurrentIndex < WorkerLimit)
+            {
+                Employees[CurrentIndex] = emp1;
+                ++CurrentIndex;
+                Console.WriteLine("Isci ugurla elave olundu");
+
+            }
+            else { Console.WriteLine("Isci limiti dolub ve buna gorede isci elave olunmadi"); }
+        }
+        public int GetEmployeeCount()
+        {
+            int count = 0;
+            foreach (var item in Employees)
+            {
+                if (item != null) ++count;
+                else break;
+            }
+            return count;
+        }
+    }
+}
+
+
